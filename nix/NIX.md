3b9717d0670112e60194d25947f1d2c609f019e1
Julien Dehos <dehos@univ-littoral.fr>
2020-01-30 14:50:57 +0100

    nix config

---
+++ nix/NIX.md
@@ -0,0 +1,67 @@
+
+# Run Polygames using Nix
+
+
+## Setup for NixOS
+
+- check that the nvidia driver is enable in `/etc/nixos/configuration.nix`:
+
+```
+  services.xserver.videoDrivers = [ "nvidia" ];
+  nixpkgs.config.allowUnfree = true;
+```
+
+
+## Setup for Nix+Linux (e.g. Debian)
+
+- install Cudatoolkit (10.1) and Cudnn
+
+- install Nix:
+
+```
+curl https://nixos.org/releases/nix/latest/install | sh
+echo "source $HOME/.nix-profile/etc/profile.d/nix.sh" >> $HOME/.bashrc
+source $HOME/.bashrc
+```
+
+
+## Build & run Polygames
+
+- activate binary cache (optional if you like compiling for hours):
+
+```
+nix-env -iA nixpkgs.cachix
+cachix use juliendehos
+```
+
+- get Polygames:
+
+```
+git clone https://github.com/facebookincubator/polygames.git
+cd Polygames
+```
+
+- run a nix-shell (one of these lines);
+
+```
+nix-shell nix/shell-cuda.nix
+nix-shell nix/shell-cpu.nix
+```
+
+- build Polygames:
+
+```
+mkdir build
+cd build
+cmake -DPYTORCH12=ON ..
+make -j4
+cd ..
+```
+
+- run Polygames:
+
+```
+python -m pypolygames train --game_name="Connect4" --device=cuda:0
+python -m pypolygames train --game_name="Connect4" --device=cpu
+```
+
---
+++ nix/shell-cuda.nix
@@ -0,0 +1,56 @@
+let
+  rev = "dfa8e8b9bc4a18bab8f2c55897b6054d31e2c71b";
+  channel = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+  config = {
+    allowUnfree = true;
+    cudaSupport = true;
+    packageOverrides = pkgs: {
+      cudatoolkit = pkgs.cudatoolkit_10_0;
+      cudnn = pkgs.cudnn_cudatoolkit_10_0;
+    };
+  };
+  pkgs = import channel { inherit config; };
+
+  python = pkgs.python3;
+  pytorch = pkgs.python3Packages.pytorchWithCuda;
+  pybind11 = pkgs.pybind11;
+
+  tensorboardX = pkgs.python3Packages.buildPythonPackage rec {
+    pname = "tensorboardX";
+    version = "1.8";
+    src = fetchTarball "https://github.com/lanpa/tensorboardX/archive/v1.8.tar.gz";
+    propagatedBuildInputs = with pkgs.python3Packages; [
+      six
+      protobuf
+      numpy
+    ];
+    doCheck = false;
+  };
+
+in pkgs.mkShell {
+  name = "Polygames-cuda";
+  src = ./.;
+
+  buildInputs = [
+    pkgs.boost
+    pkgs.cmake
+    pkgs.cudatoolkit
+    pkgs.cudnn
+    pkgs.czmq
+    pkgs.gtest
+    pkgs.linuxPackages.nvidia_x11
+    pkgs.python3Packages.pytest
+    pytorch
+    tensorboardX
+  ];
+
+  shellHook = ''
+      export CFLAGS="-I${pybind11}/include -I${pytorch}/${python.sitePackages}/torch/include -I${pytorch}/${python.sitePackages}/torch/include/torch/csrc/api/include"
+      export CXXFLAGS=$CFLAGS
+      export LDFLAGS="-L${pytorch}/${python.sitePackages}/torch/lib -L$out/${python.sitePackages} -L${pkgs.cudatoolkit}/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+      export PYTHONPATH="$PYTHONPATH:build:build/torchRL/mcts:build/torchRL/tube"
+      export CUDA_PATH=${pkgs.cudatoolkit}
+      export OMP_NUM_THREADS=1
+  '';
+}
+
