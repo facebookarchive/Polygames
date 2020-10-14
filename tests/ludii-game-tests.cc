b1eb4556bf71ba2786c2c2bc65bc4d88f9cd16b8
Dennis Soemers <d.soemers@gmail.com>
2020-10-14 13:53:53 +0200

    some more things to test

--- tests/ludii-game-tests.cc
+++ tests/ludii-game-tests.cc
@@ -63,4 +63,57 @@ TEST(LudiiGameGroup, ludii_yavalath_0) {
     ASSERT_EQ(0, features[i]);
     ++i;
   }
+  
+  // No swap occured yet, so expect full channel of 0s
+  while (i < 5 * 9 * 17) {
+    ASSERT_EQ(0, features[i]);
+    ++i;
+  }
+  
+  // Channel: Does position exist in container 0 (Board)?
+  // First and last column have 5 cells each, 
+  // expected pattern: 0,0,0,0,1,0,1,0,1,0,1,0,1,0,0,0,0
+  const float _5_cells_pattern[17] = {0,0,0,0,1,0,1,0,1,0,1,0,1,0,0,0,0};
+  for (size_t j = 0; j < 17; ++j) {
+    ASSERT_EQ(features[i + 0 * 17 + j], _5_cells_pattern[j]);
+	ASSERT_EQ(features[i + 8 * 17 + j], _5_cells_pattern[j]);
+  }
+  
+  // Second and second-to-last column have 6 cells each,
+  // expected pattern: 0,0,0,1,0,1,0,1,0,1,0,1,0,1,0,0,0
+  const float _6_cells_pattern[17] = {0,0,0,1,0,1,0,1,0,1,0,1,0,1,0,0,0};
+  for (size_t j = 0; j < 17; ++j) {
+    ASSERT_EQ(features[i + 1 * 17 + j], _6_cells_pattern[j]);
+	ASSERT_EQ(features[i + 7 * 17 + j], _6_cells_pattern[j]);
+  }
+  
+  // Third and third-to-last column have 7 cells each, 
+  // expected pattern: 0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,0
+  const float _7_cells_pattern[17] = {0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,0};
+  for (size_t j = 0; j < 17; ++j) {
+    ASSERT_EQ(features[i + 2 * 17 + j], _7_cells_pattern[j]);
+	ASSERT_EQ(features[i + 6 * 17 + j], _7_cells_pattern[j]);
+  }
+  
+  // Fourth and fourth-to-last column have 8 cells each,
+  // expected pattern: 0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0
+  const float _8_cells_pattern[17] = {0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0};
+  for (size_t j = 0; j < 17; ++j) {
+    ASSERT_EQ(features[i + 3 * 17 + j], _8_cells_pattern[j]);
+	ASSERT_EQ(features[i + 5 * 17 + j], _8_cells_pattern[j]);
+  }
+  
+  // Middle column has 9 cells,
+  // expected pattern: 1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1
+  const float _9_cells_pattern[17] = {1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1};
+  for (size_t j = 0; j < 17; ++j) {
+    ASSERT_EQ(features[i + 4 * 17 + j], _9_cells_pattern[j]);
+  }
+  i += 9 * 17;
+  
+  // All remaining channels should be all-zero; no moves played
+  while (i < 10 * 9 * 17) {
+    ASSERT_EQ(0, features[i]);
+    ++i;
+  }
 }
