4a86b7ff84f473a7aaf9ae22bc024c147f538c0a
Dennis Soemers <d.soemers@gmail.com>
2020-11-16 16:03:21 +0100

    updated Ludii unit tests

--- tests/tests.cc
+++ tests/tests.cc
@@ -9,21 +9,8 @@
 
 #include <gtest/gtest.h>
 
-#include <ludii/jni_utils.h>
-JNIEnv * JNI_ENV = nullptr;
-
 int main(int argc, char **argv) {
-    if (argc == 2) {
-        Ludii::JNIUtils::InitJVM(argv[1]);
-        JNI_ENV = Ludii::JNIUtils::GetEnv();
-        ::testing::InitGoogleTest(&argc, argv);
-        return RUN_ALL_TESTS();
-    }
-    else {
-        ::testing::InitGoogleTest(&argc, argv);
-        int res = RUN_ALL_TESTS();
-        std::cout << "WARNING: no ludii jar file specified!\n";
-        return res;
-    }
+    ::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
 
